81fc23fb5798f686d7bda1d3d486f6f57668914e
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2022-09-17 12:56:44 -0400

    adjust garden to nursery

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,33 @@
 console.log('banana');
 
-// Get time and set it to initial time
+// Get time since like noon
 const oldTime = 1663430529722;
-const d = new Date();
-let time = d.getTime() - oldTime;
-let hours = Math.floor(time/36000);
-time = time - hours*36000;
-let minutes = Math.floor(time/6000);
-time = time - minutes*6000;
-let seconds = Math.floor(time/100);
-hours = hours%24
-console.log(hours, minutes, seconds);
+
+function getTimeString(num) {
+  let numString = num.toString();
+  if (numString.length === 1) {
+    numString = '0' + numString;
+  };
+  return numString
+}
+
+function timeElapsed() {
+  const d = new Date();
+  let time = d.getTime() - oldTime;
+  let hours = Math.floor(time/3600000);
+  time = time - hours*3600000;
+  let minutes = Math.floor(time/60000);
+  time = time - minutes*60000;
+  let seconds = Math.floor(time/1000);
+  hours = hours%24;
+  let fullString = getTimeString(hours) + ':' + getTimeString(minutes) + ':' + getTimeString(seconds);
+  return fullString
+}
+// Create a string with the time variables
+const interval = setInterval(setPageTime, 1000);
+
+console.log(timeElapsed())
+function setPageTime() {
+  let str = timeElapsed();
+  document.getElementById("clock").innerHTML = str
+}
